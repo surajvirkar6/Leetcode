0fe4f0a261a54739da257eb929e1c86f0f015ec8
Suraj Virkar <87970303+surajvirkar6@users.noreply.github.com>
2022-04-21 21:51:18 +0530

    update good pairs file

--- Number_of_Good_Pairs.js
+++ Number_of_Good_Pairs.js
@@ -10,16 +10,15 @@ Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 
 let nums = [1, 2, 3, 1, 1, 3];
 let ans = function numIdenticalPairs(nums) {
-  let retAns = [];
+  let retAns = 0;
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] == nums[j]) {
-        retAns.push(i);
-        retAns.push(j);
+        retAns++;
       }
     }
   }
-  return retAns.length / 2;
+  return retAns;
 };
 console.log(ans);
 
